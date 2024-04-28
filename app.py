e5ee24f341393e26e38aa4e0d14d0feeebd783d4
Pans <caxapoker@gmail.com>
2024-04-28 15:21:54 +0700

    add registration

--- app.py
+++ app.py
@@ -1,10 +1,59 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = 'e97033b8b0e5347cb4ea4f6a1ad5d7d6'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def hello_world():
     return 'Hello world!'
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        # обработка данных отправленных через форму регистрации
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        print(username, email, password, confirm_password)
+
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            return 'Пользователь с таким именем уже существует.'
+
+        existing_email = User.query.filter_by(email=email).first()
+        if existing_email:
+            return 'Пользователь с такой почтой уже существует.'
+
+        if password != confirm_password:
+            return 'Пароль и его подтверждение не совпадают.'
+
+        new_user = User(username=username, email=email, password=password)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect('/')
+
+    return render_template('register.html')
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
